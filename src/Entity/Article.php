74d070bbdc89f2f53ee4c7f3d4f3d0a5d3ec6a47
zakiahajouni <zakiahajouni362@gmail.com>
2023-03-30 01:36:19 +0200

    Initial commit

---
+++ src/Entity/Article.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: '0')]
+    private ?string $Prix = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->Prix;
+    }
+
+    public function setPrix(string $Prix): self
+    {
+        $this->Prix = $Prix;
+
+        return $this;
+    }
+}
